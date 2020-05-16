e5dcc1914cce46c167451eb028fc0fbe17f202d3
Juan Flores-Moroco <ajaflorez.upc@gmail.com>
2020-05-16 10:48:35 -0500

    add Script 1

--- first/js/escript.js
+++ first/js/escript.js
@@ -1,3 +1,22 @@
-function saludar() {
-    alert("Hello, this is a alert")
-}
+alert("Helo Programmers");
+alert("Helo Programmers");
+alert("Helo Programmers");
+let nombre = "Carlos";
+let flotante = 2.5;
+let a = 3;
+let b = 4;
+let c = a + b;
+let d = a - b;
+let e = a * b;
+
+let verdad = true;
+let verdad2 = false;
+let verdadera = verdad || verdad2;
+
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(a == b);
+console.log(verdadera);
+
+Document.write("<p>Hello Document</p>");
