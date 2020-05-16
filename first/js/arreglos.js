502fa3fab4bb0ff2751b2a73f43ae3bb43ab9850
Juan Flores-Moroco <ajaflorez.upc@gmail.com>
2020-05-16 13:13:32 -0500

    add Script Array

--- first/js/arreglos.js
+++ first/js/arreglos.js
@@ -1,7 +1,25 @@
 // Manejo de array
-arreglo = new Array(10);
+arreglo = new Array(11);
+
 arreglo[0] = true;
 arreglo[1] = "carlos";
 arreglo[2] = 2.35;
-arreglo[3] = () => console.log("Hello");
-arreglo[3] = () => console.log("Hello");
+arreglo[3] = () => console.log("Hello Lambda");
+arreglo[4] = function () { console.log("Hello tradicional") };
+arreglo[5] = class Perro { constructor(nombre) { this.nombre = nombre} };
+
+for (let i = 6; i < 10; i++) {
+    arreglo[i] = i * 2;
+}
+
+arreglo[10] = new Array(5); // Matrices
+arreglo[10][0] = 3; // matrices
+
+for (let i = 0; i < 11; i++) {
+    console.log( arreglo[i] );
+}
+
+
+arreglo[3]();
+arreglo[4]();
+
