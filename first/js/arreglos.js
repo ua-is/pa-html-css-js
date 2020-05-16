e74e9095c00bfb6375e3141985ad95416410d24f
Juan Flores-Moroco <ajaflorez.upc@gmail.com>
2020-05-16 12:56:02 -0500

    add Script Events

---
+++ first/js/arreglos.js
@@ -0,0 +1,7 @@
+// Manejo de array
+arreglo = new Array(10);
+arreglo[0] = true;
+arreglo[1] = "carlos";
+arreglo[2] = 2.35;
+arreglo[3] = () => console.log("Hello");
+arreglo[3] = () => console.log("Hello");
--- first/js/metodos.js
+++ first/js/metodos.js
@@ -20,4 +20,51 @@ function calcular( a, b, c, d ) {
 function saludar() {
     alert("Hello Programmers");
 }
+function enfocado() {
+    console.log("Se enfoco el objeto")
+}
+function cambio() {
+    console.log("El valor cambio");
+}
+function cerrar() {
+    window.close();
+}
+function confirmar() {
+    let respuesta;
+    respuesta = confirm("¿Estas seguro que deseas hacerlo?");
+
+    if(respuesta == true)
+        console.log("Se elimino el registro");
+    else
+        console.log("Se cancelo el proceso");
+
+}
+function solicitaData() {
+    let nombre = prompt("Ingresa tu nombre");
+    console.log(nombre);
+}
+
+function calcularSuma() {
+    let num1cad, num2cad;
+    num1cad = Number(document.frmSuma.txtnum1.value);
+    num2cad = Number(document.frmSuma.txtnum2.value);
+
+    document.frmSuma.txtresultado.value = String(num1cad + num2cad);
+
+    console.log(num1cad);
+    console.log(num2cad);
+    console.log(num1cad + num2cad);
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
